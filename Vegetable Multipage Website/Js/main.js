3c36faa353138fc1548db4428957c4f0b6fb832e
Pallab Roy Tushar <112120095+pallabroy2234@users.noreply.github.com>
2022-12-18 08:04:33 -0800

    Add files via upload

---
+++ Vegetable Multipage Website/Js/main.js
@@ -0,0 +1,21 @@
+// Header Scroll
+
+let nav = document.querySelector(".navbar");
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 50) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+};
+
+// Nabbar hide
+
+let navbar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+
+navbar.forEach((e) => {
+  e.addEventListener("click", () => {
+    navCollapse.classList.remove("show");
+  });
+});
